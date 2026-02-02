2e5014487a0efa60be3399cd3164e628e53a6b7e
dklobov <135457230+dklobov@users.noreply.github.com>
2026-02-02 21:42:41 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-const buttonWantHere = document.querySelector('.button-want-here');
+const buttonSearch = document.querySelector('.button-search');
 const modal = document.querySelector('.modal');
 const modalCloseButton = document.querySelector('.modal-close-button');
 
-buttonWantHere.addEventListener('click', (evt) => {
+buttonSearch.addEventListener('click', (evt) => {
     evt.preventDefault();
     modal.classList.remove('modal-closed');
 })
